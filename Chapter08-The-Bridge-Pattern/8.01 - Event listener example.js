00d63c9e85090b1318615e2dd2865b1895706ef3
arthinking <pzx@itzhai.com>
2012-11-07 13:51:17 +0800

    add note

--- Chapter08-The-Bridge-Pattern/8.01 - Event listener example.js
+++ Chapter08-The-Bridge-Pattern/8.01 - Event listener example.js
@@ -7,6 +7,8 @@ function getBeerById(e) {
   });
 }
 
+// 把事件处理的语句封装到回调函数中，现在我们通过接口而不是实现进行编程
+// getBeerById并没有和事件对象捆绑在一起。
 function getBeerById(id, callback) {
   // Make request for beer by ID, then return the beer data.
   asyncRequest('GET', 'beer.uri?id=' + id, function(resp) {
@@ -14,7 +16,7 @@ function getBeerById(id, callback) {
     callback(resp.responseText);
   });
 }
-
+// 桥接器的使用：事件监听器回调函数
 addEvent(element, 'click', getBeerByIdBridge);
 function getBeerByIdBridge (e) {
   getBeerById(this.id, function(beer) {
